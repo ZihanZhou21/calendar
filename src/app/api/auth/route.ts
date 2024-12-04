4a69c10ad898359a1d5e5a210823b71e474f12ec
Zihan <zihanzhou1021@gmail.com>
2024-12-04 13:33:04 +1100

    complete calendar

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -4,6 +4,9 @@ import { nylas, nylasConfig } from '@/app/libs/nylas'
 import { redirect } from 'next/navigation'
 export async function GET() {
   // Route to initialize authentication
+  if (!nylasConfig.clientId || !nylasConfig.callbackUri) {
+    throw new Error('Missing required Nylas configuration')
+  }
 
   const authUrl = nylas.auth.urlForOAuth2({
     clientId: nylasConfig.clientId,
