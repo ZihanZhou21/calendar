5f77d0e9114a97fcc04c09d0da2daae005894282
Zihan <zihanzhou1021@gmail.com>
2024-12-20 12:24:43 +1100

    fix warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizeCss: true, // 确保开启优化CSS
+  },
+}
 
-export default nextConfig;
+export default nextConfig
